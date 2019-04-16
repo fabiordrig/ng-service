89d1be7e9b0d9ab039425a2628017646b21ffcec
fabiordrig <fabiordrig@gmail.com>
2019-04-16 09:49:34 -0300

    1 file modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { FormComponent } from './form/form.component';
+import { TabelaComponent } from './tabela/tabela.component';
+import { ServicoService } from './servico.service';
+import { ClienteService } from './cliente.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HelloComponent, FormComponent, TabelaComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [ServicoService, ClienteService]
 })
 export class AppModule { }
---
+++ src/app/cliente.ts
@@ -0,0 +1,7 @@
+export class Cliente {
+  static cont:number =0;
+  id:number = ++Cliente.cont;
+  nome:string;
+  idade: number;
+
+}
